7426da045bba92f1ced21603adc9f58dbff5a6bb
unknown <janbenjaminmesina@gmail.com>
2023-02-18 18:08:42 +0800

    sidebar v1

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -64,7 +64,7 @@ export const Login: React.FC = () => {
           }}
         >
           <div>
-            <img src={Arctos} alt="Arctos Logo" />
+            <img src={ Arctos } alt="Arctos Logo" />
           </div>
           <Box mt={4}>
             <GoogleButton />
